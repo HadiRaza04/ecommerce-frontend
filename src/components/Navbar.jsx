e38d361b31ef5c3565c321ef77bdfaa1a5e439d3
Hadi Raza <hadiraza.edu@gmail.com>
2026-03-20 12:00:54 +0500

    show admin dashboard to admins only

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AiOutlineClose, AiOutlineMenu, AiOutlineSearch, AiFillTag } from 'react-icons/ai'
 import { BsFillCartFill, BsFillSaveFill } from 'react-icons/bs'
 import { TbTruckDelivery } from 'react-icons/tb'
-import { MdFavorite, MdHelp } from 'react-icons/md'
+import { MdFavorite, MdHelp, MdOutlineAdminPanelSettings } from 'react-icons/md'
 import { FaWallet, FaUserFriends } from 'react-icons/fa'
 import { MdLogout } from "react-icons/md";
 import Cart from './Cart'
@@ -14,6 +14,8 @@ function Navbar() {
     const [nav, setNav] = useState(false);
     const [cart, setCart] = useState(false);
     const { isLogin, setIsLogin } = useContext(CartContext);
+    const navigate = useNavigate();
+    const user = JSON.parse(localStorage.getItem('user'));
 
   return (
     <div className='    max-w-[1640px] mx-auto flex justify-between items-center p-4 sticky top-0 z-[1] bg-white shadow-md'>
@@ -56,19 +58,36 @@ function Navbar() {
             <h2 className='text-2xl p-4'>Shopping <span className='font-bold'>Center</span></h2>
             <nav>
                 <ul className='flex flex-col p-4 text-gray-800'>
-                    <li className='text-xl py-4 flex'><TbTruckDelivery size={ 25 } className='mr-4'/> Orders</li>
-                    <li className='text-xl py-4 flex'>
+                    {user && user.role === 'admin' && (
+                        <li className='text-xl py-4 flex cursor-pointer text-blue-600 font-bold' onClick={() => {
+                            navigate("/dashboard")
+                            setNav(false)
+                        }}><MdOutlineAdminPanelSettings size={ 25 } className='mr-4'/> Admin Panel</li>
+                    )}
+                    <li className='text-xl py-4 flex cursor-pointer' onClick={() => {
+                        navigate("/orders")
+                        setNav(false)
+                    }}><TbTruckDelivery size={ 25 } className='mr-4'/> Orders</li>
+                    <li className='text-xl py-4 flex cursor-pointer' onClick={() => {
+                        navigate("/favorites")
+                        setNav(false)
+                    }}>
                         
                         <MdFavorite  size={ 25 } className='mr-4'/> 
                         Favorite
                     </li>
+                    <li className='text-xl py-4 flex cursor-pointer' onClick={() => {
+                        navigate("/wallet")
+                        setNav(false)
+                    }}>
+                    <FaWallet size={ 25 } className='mr-4'/>
+                     Wallet
+                    </li>
+                    
                     <li className='text-xl py-4 flex'><MdHelp size={ 25 } className='mr-4'/> Help</li>
                     <li className='text-xl py-4 flex'><AiFillTag size={ 25 } className='mr-4'/> Promotions</li>
                     <li className='text-xl py-4 flex'><BsFillSaveFill size={ 25 } className='mr-4'/> Best Ones</li>
                     <li className='text-xl py-4 flex'><FaUserFriends size={ 25 } className='mr-4'/> Invite Friends</li>
-                    <li className='text-xl py-4 flex'><FaWallet size={ 25 } className='mr-4'/>
-                     Wallet
-                    </li>
                     <li className='text-xl py-4 flex cursor-pointer' onClick={() => {
                         setCart(!cart)
                         setNav(!nav)
